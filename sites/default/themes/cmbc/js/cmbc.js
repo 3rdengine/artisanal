5f322f97280a93f119adedf654cbadee23bef5b6
Robert Flach <robert@point-of-rental.com>
2015-10-10 16:26:20 -0500

    adding the rest of the bootstrap js.

--- sites/default/themes/cmbc/js/cmbc.js
+++ sites/default/themes/cmbc/js/cmbc.js
@@ -1,3 +1,15 @@
 jQuery(document).ready(function(){
+  jQuery.getScript('../sites/default/themes/cmbc/bootstrap/js/affix.js');
+  jQuery.getScript('../sites/default/themes/cmbc/bootstrap/js/alert.js');
+  jQuery.getScript('../sites/default/themes/cmbc/bootstrap/js/button.js');
+  jQuery.getScript('../sites/default/themes/cmbc/bootstrap/js/carousel.js');
+  jQuery.getScript('../sites/default/themes/cmbc/bootstrap/js/collapse.js');
+  jQuery.getScript('../sites/default/themes/cmbc/bootstrap/js/modal.js');
+  jQuery.getScript('../sites/default/themes/cmbc/bootstrap/js/tooltip.js');
+  jQuery.getScript('../sites/default/themes/cmbc/bootstrap/js/popover.js');
+  jQuery.getScript('../sites/default/themes/cmbc/bootstrap/js/scrollspy.js');
+  jQuery.getScript('../sites/default/themes/cmbc/bootstrap/js/tab.js');
+  jQuery.getScript('../sites/default/themes/cmbc/bootstrap/js/transition.js');
   jQuery.getScript('../sites/default/themes/cmbc/bootstrap/js/dropdown.js');
+
 });
