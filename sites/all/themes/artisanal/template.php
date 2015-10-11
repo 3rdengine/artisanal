bdc5964e62b4b7e82c4ba32d23e917f089ceae1d
Tony Vance <tony.vance@latentcodex.com>
2015-10-11 01:12:15 +0000

    adding funky triangle thing

--- sites/all/themes/artisanal/template.php
+++ sites/all/themes/artisanal/template.php
@@ -4,3 +4,11 @@
  * @file
  * template.php
  */
+
+
+/**
+ * Bootstrap theme wrapper function for the primary menu links.
+ */
+function artisanal_menu_tree__primary(&$variables) {
+  return '<ul class="menu nav navbar-nav primary-nav">' . $variables['tree'] . '</ul>';
+}
